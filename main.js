becba5204cc9a17c962e1ac7037795b311766118
Danny <byxdenpro@gmail.com>
2024-06-27 09:40:56 +0300

    my jQuery slider

--- main.js
+++ main.js
@@ -1,7 +1,47 @@
 $(document).ready(function(){
-    $('.carousel').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        centerMode: true,
-    });
-});
+  
+    var interval = window.setInterval(rotateSlides, 3000)
+    
+    //Function to rotate the slides every 1 second automatically
+    function rotateSlides(){
+      var $firstSlide = $('.carousel').find('div:first');
+      var width = $firstSlide.width();
+      
+      $firstSlide.animate({marginLeft: -width}, 1000, function(){
+        var $lastSlide = $('.carousel').find('div:last')
+        $lastSlide.after($firstSlide);
+        $firstSlide.css({marginLeft: 0})
+      })
+    }
+  
+    //Listens for the click event on the right or left arrow
+    $('#left').click(previousSlide);
+    $('#right').click(nextSlide);
+    $('.slide-image').click(nextSlide);
+
+    //Function to move to the next slide
+    function nextSlide(){
+        window.clearInterval(interval);
+        var $currentSlide = $('.carousel').find('div:first');
+        var width = $currentSlide.width();
+        $currentSlide.animate({marginLeft: -width}, 1000, function(){
+            var $lastSlide = $('.carousel').find('div:last')
+            $lastSlide.after($currentSlide);
+            $currentSlide.css({marginLeft: 0})
+            interval = window.setInterval(rotateSlides, 3000);
+        });
+    }
+
+    //Function to move to the previous slide
+    function previousSlide(){
+        window.clearInterval(interval);
+        var $currentSlide = $('.carousel').find('div:first');
+        var width = $currentSlide.width();
+        var $previousSlide = $('.carousel').find('div:last')
+        $previousSlide.css({marginLeft: -width})
+        $currentSlide.before($previousSlide);
+        $previousSlide.animate({marginLeft: 0}, 1000, function(){
+            interval = window.setInterval(rotateSlides, 3000);
+        });
+    }
+  })
